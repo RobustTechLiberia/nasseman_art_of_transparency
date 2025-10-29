b38dcb089e0b4c6c9b1b30c81691dd3d5ea1dcc4
Gabriel W Kun <gabrielwkun@192.168.1.38>
2025-10-29 11:24:04 +0000

    test 2

--- src/components/section_2.jsx
+++ src/components/section_2.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import img from '../assets/images/WhatsApp Image 2025-10-13 at 8.14.08 PM.jpeg'
+import $ from "jquery";
 
 class Sec extends React.Component {
 
+    componentDidMount(){
+
+        $(document).ready(function() {
+
+  $('span#readmore').hide();
+
+  $("#btn").click(function() {
+    $('span#readmore').toggle();
+  });
+
+});
+
+    }
    
     render() {
 
@@ -15,11 +29,16 @@ class Sec extends React.Component {
                     <div className="mt-8 md:mt-8 md:mx-5 mx-5">
                        <p className="text-left font-sans font-normal normal-case text-lg">
                          Nasseman Art For Transparency (NAFT) is a Non-Governmental Organization (NGO) founded by Mr. Rabbie Nassrallah, popularly known as NASSEMAN, a Global Music Anti- Corruption Ambassador for Transparency International and FairPlay Music.
+                         <span id="readmore">
+                            Nasseman Art For Transparency is on a mission to empower young people to raise their voices against corruption and injustice through various artistic mediums such as education, music, art, drama,
+and films.
+                         </span>
+                         <button className="border-none bg-white text-left  py-3 text-red-900 font-sans normal-case mx-0 hover:underline cursor-pointer px-1" id="btn"> Read more</button>
                        </p>
 </div>      
                      </div>
-                    <div className="w-80 h-96 bg-black md:mt-5 mt-3">
-                        <img src={img} alt="Nasseman" className="w-full bg-cover h-full" />
+                    <div className="w-80 h-96 bg-black md:mt-5 mt-3 rounded-lg">
+                        <img src={img} alt="Nasseman" className="w-full bg-cover h-full rounded-lg hover:bg-blend-overlay hover:opacity-50 cursor-pointer" />
                     </div>
                 </div>
             </>
