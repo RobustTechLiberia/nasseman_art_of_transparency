15a56c16cfa55ef9656a44ef86b989ed3b10f303
Gabriel W Kun <gabrielwkun@Gabriels-MacBook-Pro.local>
2025-10-29 13:49:33 +0000

    test 2

--- src/components/partenership.jsx
+++ src/components/partenership.jsx
@@ -11,9 +11,9 @@ class Partenership extends React.Component {
 
            <>
     <div id="partnership">
-  <div className="bg-black/80 bg-blend-overlay h-96 w-auto mt-60 flex flex-col justify-center items-center bg-cover bg-center bg-no-repeat object-cover inset-0 z-0">
+  <div className="bg-black/80 bg-blend-overlay h-auto md:h-96 w-auto mt-60 flex flex-col justify-center items-center bg-cover bg-center bg-no-repeat object-cover inset-0 z-0">
     
-    <h1 className="text-white text-4xl font-sans font-semibold mt-2 uppercase mx-5 md:w-xl text-center">
+    <h1 className="text-white text-4xl font-sans font-semibold mt-5 uppercase mx-5 md:w-xl text-center">
       become a partner<br />against corruption
     </h1>
 
@@ -21,7 +21,7 @@ class Partenership extends React.Component {
       Join us, support our Movement! and let’s help educate the next generation about the ripple effects of corruption and injustice in our society.
     </p>
 
-    <button className="bg-red-900 py-3 px-8 rounded-sm text-white capitalize text-2xl cursor-pointer hover:bg-red-800 transition">
+    <button className="bg-red-900 py-3 px-8 rounded-sm text-white capitalize text-2xl cursor-pointer hover:bg-red-800 transition mb-8">
       <FontAwesomeIcon icon={faFileDownload} className="text-2xl" /> register
     </button>
 
