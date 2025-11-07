5d6ec75e3e5e8fc46b7d7832b9eef3252f9f0e87
Gabriel W Kun <gabrielwkun@192.168.1.22>
2025-11-07 14:16:34 +0000

    test 6

---
+++ src/components/sponsor.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Sponsorship extends React.Component{
+
+    render(){
+
+        return(
+
+            <>
+            <div className="bg-white h-96 mt-24">
+                <h1 className="text-center font-sans md:text-4xl text-3xl capitalize font-semibold">
+                    our partners
+                </h1>
+            </div>
+            </>
+        )
+    }
+}
+
+export default Sponsorship;
